12274cd41ba09c937109db1afb0e7621786ea260
naomital <naomilau699@gmail.com>
2018-11-21 20:55:19 +0200

    new

---
+++ src/try3/ChatMessage.java
@@ -0,0 +1,39 @@
+package try3;
+
+import java.io.*;
+/*
+ * This class defines the different type of messages that will be exchanged between the
+ * Clients and the Server. 
+ * When talking from a Java Client to a Java Server a lot easier to pass Java objects, no 
+ * need to count bytes or to wait for a line feed at the end of the frame
+ */
+public class ChatMessage implements Serializable {
+
+	protected static final long serialVersionUID = 1112122200L;
+
+	// The different types of message sent by the Client
+	// WHOISIN to receive the list of the users connected
+	// MESSAGE an ordinary message
+	// LOGOUT to disconnect from the Server
+	static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2, IM = 3;
+	private int type;
+	private String message;
+	
+	// constructor
+	ChatMessage(int type, String message) {
+		this.type = type;
+		this.message = message;
+		if(message.startsWith("@")) {
+			this.type = IM;
+			this.message = message.substring(1);
+		}
+	}
+	
+	// getters
+	int getType() {
+		return type;
+	}
+	String getMessage() {
+		return message;
+	}
+}
